b7e432a5032c032aaddad31e294a28e937d23c9e
Frozouse <vladimir.stukanov@gmail.com>
2026-03-11 17:56:07 +0700

    Вычисление круга

---
+++ figures.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int ring(int r,int ang){
+  std::cout << "Длина окружности: " << std::endl;
+
+  std::cout << r*2*3.14 << std::endl;
+  std::cout << "Площадь круга: " << std::endl;
+  std::cout << r*3.14*3.14<< std::endl;
+  std::cout << "Площадь кругового сектора: " << std::endl;
+  std::cout << r*3.14*3.14*ang/360 << std::endl;
+
+  return 0;
+}
+
+int main(){
+  unsigned int in_r, in_ang;
+  while (1){
+    std::cout << "Введите радиус круга: " << std::endl;
+    std::cin >> in_r;
+    std::cout << "Введите угол сектора в градусах: " << std::endl;
+    std::cin >> in_ang;
+    if (in_r > 0 && 0 <= in_ang && in_ang <= 360){
+      break;
+    }
+    else{
+      std::cout << "Введены невозможные параметры. Попробуйте еще раз: "<< std::endl;
+    }
+  }
+  ring(in_r,in_ang);
+  return 0;
+}
